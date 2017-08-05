60954f1f28f759e2082caf871bb1d3a4c8d3fc67
nhatl <nhatl@DESKTOP-G4099EE>
2017-08-05 15:45:50 +1000

    Submit the first commit!

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var message = new MessageModel();
+            message.welcome = "HELLO GITHUB!!";
+            return View(message);
+        }
+    }
+}
